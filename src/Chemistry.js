20e773d54783abb5a7a5e2cc4fb113adf06efac5
Ryan Chow <ccchowww@gmail.com>
2017-11-25 10:39:55 +0800

    Chem finished

--- src/Chemistry.js
+++ src/Chemistry.js
@@ -5,15 +5,33 @@ class Chemistry extends React.Component {
    return (
      <div>
        <h1>Chemistry</h1>
-       <h2>Paper 4</h2>
-       <p>2017</p>
-       <ul>
-         <li class="description"><a class="qp" href="https://drive.google.com/open?id=1USSNvzLMjU8udu0RhgJpthepFrMaywoX" target="_blank">March 42 QP</a><a class="ms" href="https://drive.google.com/open?id=1kGJUdVuhgX1aVuney3zeGrUIYPk4O0lW" target="_blank">MS</a></li>
-         <li>gdrive links</li>
-         <li>gdrive links</li>
-         <li>gdrive links</li>
-         <li>gdrive links</li>
-       </ul>
+          <h2>Paper 4</h2>
+            <ul>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1tfEdcj11elbfvpSpCxVQfSNGeKeQohY8" target="_blank">2017 March 42 QP</a><a class="ms" href="https://drive.google.com/open?id=17DAABSAHZNzYuKrxDBcLpd4p6ZvQEFCT" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1sQIEDEyJsH52q6aSZBWMW2Aomg7GYSXV" target="_blank">2017 June 41 QP</a><a class="ms" href="https://drive.google.com/open?id=1ituVDK1SSP4zFvmA2YwjJybmrEn5Fh0j" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1M5fCwUtf2kDccJOa7qaArQjZ8xWtZq6N" target="_blank">2016 March 42 QP</a><a class="ms" href="https://drive.google.com/open?id=1wewk-GnJg65ucwZkIawsuHureTapEMNq" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=10aPZqyr3o_KIm3d7OXQtC_-CC6GkCkaU" target="_blank">2016 June 41 QP</a><a class="ms" href="https://drive.google.com/open?id=1q8Fzshn7u2A89ysggk14Q4HU7tX6zX18" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=18z68_zRDSy1zd_DMHjx8g1ozoWYBWXqD" target="_blank">2016 November 41 QP</a><a class="ms" href="https://drive.google.com/open?id=1zTIAVKrq41cO8bU3krFD9u4vMjE7q2uh" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1KZ3b0w_K_ybqTGfq-LfJTqfTI8h_vaHo" target="_blank">2015 June 41 QP</a><a class="ms" href="https://drive.google.com/open?id=17Hclzvyc234fyCYdvK1vzBwxqPwc6EZ5" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1HP-aew9kmZkz8svVy-AEUyZH_yp7ZO0-" target="_blank">2014 June 41 QP</a><a class="ms" href="https://drive.google.com/open?id=16xRVvt17VFiqVcHXelEjMbzfLjb3P54Y" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1dAqWiFpAS1k5Nqzsm7Li1yTnpsRqJ7jt" target="_blank">2014 November 41 QP</a><a class="ms" href="https://drive.google.com/open?id=1U81WECNLc7VGIp9cvaOk59I2MYENg5RP" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=13bmA1zSGCOAXWMaXkAOwbNbi0b7re2Cx" target="_blank">2013 June 41 QP</a><a class="ms" href="https://drive.google.com/open?id=1fUYOv-8Ikr2C82AVbH_rPDoQNvGaJZa0" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1QunBjthnGl0PalU5u6dE6UomRTIjStAB" target="_blank">2013 November 41 QP</a><a class="ms" href="https://drive.google.com/open?id=1Dj9AIVylCXB94V4cvqaQhvAn1YbzaPUO" target="_blank">MS</a></li>
+              <li class="description"><a class="qp" href="https://drive.google.com/open?id=1oc6kfxytFOonp3c6Q5Hq7COAuCkj1Qgz" target="_blank">2012 November 41 QP</a><a class="ms" href="https://drive.google.com/open?id=1TZ7K4VRhYWqjlVnQgF5x7ccKCNRJYoFJ" target="_blank">MS</a></li>
+            </ul>
+          <h2>Paper 5</h2>
+            <ul>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1ZZdO9YnqT2sLIiJJl5aKkZCBLVxOQ-0P" target="_blank">2017 March 52 QP</a><a class="ms" href="https://drive.google.com/open?id=1cyY3ZTm94q0_yy9FHVgF4qm8eokpO8ZV" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1CNyl7jlexhXroikr8J9OIkMl1aah6Uak" target="_blank">2017 June 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1fM9njM9YQh0yNCuxtOD-fEckSlAyWxWU" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1JkwOpP3NsBchXm1JFLdTva_wTJQZ-eBG" target="_blank">2016 March 52 QP</a><a class="ms" href="https://drive.google.com/open?id=1RctspI8BfI6zQkbj_jPhomGbkOe0_1fF" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1BKeo9wZuJdXB-QVFKChFzZ5GAT342mS_" target="_blank">2016 June 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1THPeqnFsWJUceG5OfWn_8bOzKuj0EhhS" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1Jv9GQnLCuskYr1rJtG9gli__XwMMAXX9" target="_blank">2016 November 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1EkqoxzleNxZP2e-B2iSodOmeA33vHncX" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1nqtcXgBuAjZ4tzRY8rMzoTNpqS2uIi3B" target="_blank">2015 June 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1cgklsYGRyS34aYxcHfTjvvUCjJEpGX3z" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1BgpVaWvAp2Jf0atxu8zxwzTmLudv5gkK" target="_blank">2014 June 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1kIqscdcfcABw-pSuNKf0zVtRcFQRgl7r " target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1B0AwocbguKxA0fGvEgtJupvYqOdcMJqS" target="_blank">2014 November 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1BinKq3_8x7uDYvlLZvII3bucq4P8j3c9" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1k6paXSFzga7CswRJ8P0qThJkMjsWbQsK" target="_blank">2013 June 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1a-weW1ofxw8xNaWqP7IEtoS4fxdfx-Sh" target="_blank">MS</a></li>
+            <li class="description"><a class="qp" href="https://drive.google.com/open?id=1YJvEph2LcNL37dEwNfCc3ESjIaiHSed1" target="_blank">2013 November 51 QP</a><a class="ms" href="https://drive.google.com/open?id=1iTPP4K1V6jfYlNF18kOmRVtZYOJRNcmP" target="_blank">MS</a></li>
+            </ul>
      </div>
    );
  }
