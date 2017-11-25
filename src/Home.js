2ccf82c14d2f410cf215eb01fbdcd0eaf408affc
Ryan Chow <ccchowww@gmail.com>
2017-11-25 23:02:50 +0800

    completed before deply

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
 class Home extends React.Component {
  render() {
    return (
      <div>
-       
+       <img src={require("./puginablanket.jpg")} alt="pug in a blanket"/>
      </div>
    );
  }
